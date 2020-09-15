3bd478976b563cbae93b6b61a70bf9db6e6c5fec
Chris Rees <crees@FreeBSD.org>
2020-09-15 19:45:57 +0100

    Typo!

--- clear_cache.php
+++ clear_cache.php
@@ -4,7 +4,7 @@ namespace Roombooking;
 
 require "bin/classes.php";
 
-if (!array_search($auth_user, Config::admin_users) {
+if (!array_search($auth_user, Config::admin_users)) {
     header("location: denied.php");
     die();
 }
