53e8f4fbc2d5c19452c8b89d4f89b32e8c9c1e01
Chris Rees <crees@FreeBSD.org>
2020-11-16 11:48:01 +0000

    Favicon

--- bin/head.php
+++ bin/head.php
@@ -2,6 +2,8 @@
 
 <title><?= Config::site ?></title>
 
+<link rel="icon" href="<?= Config::site_url; ?>/img/badge_blue.png">
+
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 <link rel="stylesheet" href="<?=Config::site_url;?>/css/custom.css">
 
