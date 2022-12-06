600937a5132445b676cb16bc2918014d8e50fff8
Chris Rees <crees@FreeBSD.org>
2022-12-06 12:31:43 +0000

    Don't force redirect to HTTPS

--- bin/classes.php
+++ bin/classes.php
@@ -4,11 +4,6 @@ namespace Roombooking;
 
 /* Have we been called before? */
 if (!class_exists("Config")) {
-    if(empty($_SERVER['HTTPS']) || $_SERVER['HTTPS'] == "off") {
-        header('HTTP/1.1 301 Moved Permanently');
-        header('location: https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']);
-        exit();
-    }
     require "Config.php";
     require "Database.php";
     // Don't collide with settings.php
